fff75fd1333120bdffa20d8ae0903e42f36435d4
penguinz <penguinz@protonmail.com>
2019-06-22 15:43:00 +0300

    add paginator

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import News
 
 
 def index(request):
-    news_list = News.objects.order_by('-pub_date')[:5]
+    news_list = News.objects.all()[::-1]
+    paginator = Paginator(news_list, 3)
+    page = request.GET.get('page')
+    news = paginator.get_page(page)
     context = {
-        'news_list': news_list
+        'news_list': news,
     }
+
     return render(request, 'blog/index.html', context)
 
